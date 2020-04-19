d27a423eaa6fcfb1ec3beace71f2b27646928779
Aaquib Ahmed <aaquibskimbox@gmail.com>
2020-04-20 01:37:44 +0530

    added todolist function

---
+++ app.js
@@ -0,0 +1,38 @@
+//Selectors
+const todoInput = document.querySelector('.todo-input');
+const todoButton = document.querySelector('.todo-button');
+const todoList = document.querySelector('.todo-list');
+
+
+//Event listners
+todoButton.addEventListener('click', addTodo);
+
+
+//Functions
+function addTodo(e){
+    //prevent form from submitting
+    event.preventDefault();
+
+    // Todo div
+    const todoDiv = document.createElement('div');
+    todoDiv.classList.add('todo');
+
+    // Create li
+    const newTodo = document.createElement('li');
+    newTodo.innerText = 'hey';
+    newTodo.classList.add('todo-item');
+    todoDiv.appendChild(newTodo);
+    
+    // check mark button
+    const completedButton = document.createElement('button');
+    completedButton.innerHTML = "<i class='fas fa-check'></i>";
+    todoDiv.appendChild(completedButton);
+
+    // check trash button
+    const trashButton = document.createElement('button');
+    trashButton.innerHTML = "<i class='fas fa-trash'></i>";
+    todoDiv.appendChild(trashButton);
+
+    // append to list
+    todoList.appendChild(todoDiv);
+}
