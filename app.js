14f75b8b80638a22c760c508334ccccce3dd27c3
Aaquib Ahmed <aaquibskimbox@gmail.com>
2020-04-20 14:06:35 +0530

    added filter options

--- app.js
+++ app.js
@@ -2,11 +2,13 @@
 const todoInput = document.querySelector('.todo-input');
 const todoButton = document.querySelector('.todo-button');
 const todoList = document.querySelector('.todo-list');
+const filterOptions = document.querySelector('.filter-todo')
 
 
 //Event listners
 todoButton.addEventListener('click', addTodo);
 todoList.addEventListener('click', deleteCheck);
+filterOptions.addEventListener('click', filterTodo);
 
 
 //Functions
@@ -63,4 +65,31 @@ function deleteCheck(e) {
         const todo = item.parentElement;
         todo.classList.toggle('completed');
     }
+}
+
+function filterTodo(e) {
+    const todos = todoList.childNodes;
+    todos.forEach(function(todo){
+        switch(e.target.value) {
+            case 'all':
+                todo.style.display = 'flex';
+                break;
+
+            case 'completed':
+                if(todo.classList.contains('completed')){
+                    todo.style.display = 'flex';
+                } else {
+                    todo.style.display = 'none';
+                }
+                break;
+            
+            case 'uncompleted':
+                if(!todo.classList.contains('completed')){
+                    todo.style.display = 'flex';
+                } else {
+                    todo.style.display = 'none';
+                }
+                break;
+        }
+    });
 }
